d13ac69a92d83505f238f0ad980d84bbacb30585
Gabriel Luis <gabriel.luis@dextra-sw.com>
2021-03-07 02:54:17 -0300

    Execute forever tweeting once a day

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 require('dotenv').config()
-const { deepGet } = require('deep-getter')
+
 const { TwitterClient } = require('twitter-api-client')
+const { deepGet } = require('deep-getter')
+
+const { compose } = require('./utils/compose')
+const { forever } = require('./utils/forever')
+const { getStatus } = require('./utils/getStatus')
+const { tweet } = require('./utils/tweet')
+const { waitOneDay } = require('./utils/waitOneDay')
 
 const twitterClient = new TwitterClient({
-    apiKey: deepGet(process, 'env.TWITTER_API_KEY'),
-    apiSecret: deepGet(process, 'env.TWITTER_API_SECRET'),
-    accessToken: deepGet(process, 'env.TWITTER_ACCESS_TOKEN'),
-    accessTokenSecret: deepGet(process, 'env.TWITTER_ACCESS_TOKEN_SECRET')
+  apiKey: deepGet(process, 'env.TWITTER_API_KEY'),
+  apiSecret: deepGet(process, 'env.TWITTER_API_SECRET'),
+  accessToken: deepGet(process, 'env.TWITTER_ACCESS_TOKEN'),
+  accessTokenSecret: deepGet(process, 'env.TWITTER_ACCESS_TOKEN_SECRET')
 })
 
-twitterClient.tweets.statusesUpdate({
-    status: 'No'
-}).then (response => {
-    console.log('Tweeted!', response)
-}).catch(error => {
-    console.error(error)
-})
+const tweetStatus = compose(tweet(twitterClient), getStatus)
+const tweetAndWait = compose(waitOneDay, tweetStatus)
+
+forever(tweetAndWait)
